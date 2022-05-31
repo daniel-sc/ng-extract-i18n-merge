0368e85cce53945cf7453d57fc4c30a4191ef976
Daniel Schreiber <daniel-schreiber@gmx.de>
2022-05-31 18:16:30 +0200

    migrations fixed

--- schematics/migrations/2_0_0/index.ts
+++ schematics/migrations/2_0_0/index.ts
@@ -28,16 +28,17 @@ export default function (): Rule {
                 const i18nMergeTarget = project.targets.get('extract-i18n-merge');
                 if (i18nMergeTarget) {
                     context.logger.info(`updating extract-i18n(-merge) targets for project ${projectName}..`)
-                    project.targets.delete('extract-i18n-merge');
-                    const i18nTarget = project.targets.get('extract-i18n') ?? {builder: 'ng-extract-i18n-merge:ng-extract-i18n-merge'};
+                    const i18nTarget = {...(project.targets.get('extract-i18n') ?? {builder: 'ng-extract-i18n-merge:ng-extract-i18n-merge'})};
                     i18nTarget.builder = 'ng-extract-i18n-merge:ng-extract-i18n-merge';
                     i18nTarget.options = {
                         ...i18nMergeTarget.options,
                         browserTarget: i18nTarget.options?.browserTarget ?? `${projectName}:build`
                     }
-                    context.logger.info(`setting extract-i18n target to: ${JSON.stringify(i18nTarget)}`);
-                    project.targets.set('extract-i18n', {...i18nTarget});
-                    context.logger.info(`project.targets: ${JSON.stringify(Array.from(project.targets.entries()))}`);
+                    project.targets.delete('extract-i18n'); // 'project.targets.set' not working!?
+                    project.targets.add({name: 'extract-i18n', ...i18nTarget});
+                    project.targets.delete('extract-i18n-merge');
+                } else {
+                    context.logger.warn('target "extract-i18n-merge" not found in workspace - please migrate manually!');
                 }
             });
         });
