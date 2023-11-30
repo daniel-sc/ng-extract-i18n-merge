c26e9d6d363e4c59d170d69c6c876bab7abbce2b
Michael Wild <150654170+michaelwildvarian@users.noreply.github.com>
2023-11-30 23:23:22 +0100

    review: Less greedily remove space

--- src/stringUtils.ts
+++ src/stringUtils.ts
@@ -1,10 +1,7 @@
+// Converts sequences of space, tabs and "section breaks" like linefeeds and
+// carriage returns to a single space, but leaving other space characters like
+// no-break-space (U+00A0) intact.
 // Refer to https://developer.mozilla.org/en-US/docs/Web/CSS/white-space-collapse#collapsing_of_white_space
 export function doCollapseWhitespace<T extends string | undefined>(destSourceText: T): T {
-    let result = destSourceText
-        ?.replace(/\t+/g, ' ')              // Convert tabs to spaces
-        ?.replace(/(\r\n|\n|\r)+/g, ' ')    // Collapse "sequences of segment breaks" to a single space (no need to preserve line-breaks)
-        ?.replace(/ *([^\S ]+) */g, '$1')   // Remove space before or after a "space-like" character that is not a space (e.g. no-break space).
-        ?.replace(/ {2,}/g, ' ');           // Collapse sequences of spaces to a single space
-
-    return result as T;
+    return destSourceText?.replace(/[\n\r\t ]+/g, ' ') as T;
 }
