b29d6541fab1b3aaf151d2421a50d69be56b6496
Daniel Schreiber <daniel-schreiber@gmx.de>
2024-02-07 22:02:55 +0100

    fix: build error: A jest worker process was terminated by another process: signal=SIGSEGV,

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,8 @@ const config: JestConfigWithTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: false,
+    maxWorkers: 10,
+    maxConcurrency: 10,
     testMatch: undefined,
     testRegex: '.*\.spec\.ts$',
     collectCoverageFrom: ['src/**/*.ts'], // exclude coverage from schematic as it only collects from js (instead of ts)..
