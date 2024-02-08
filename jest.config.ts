4ecf3cda341a1bb425be4eb7dea3e46f6ef04c07
Daniel Schreiber <daniel-schreiber@gmx.de>
2024-02-08 15:23:08 +0100

    fix: another try for successfully running CI pipeline..

--- jest.config.ts
+++ jest.config.ts
@@ -5,12 +5,12 @@ const config: JestConfigWithTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: false,
-    maxWorkers: 10,
-    maxConcurrency: 10,
+    maxWorkers: 100,
+    maxConcurrency: 100,
+    testTimeout: 30_000,
     testMatch: undefined,
     testRegex: '.*\.spec\.ts$',
     collectCoverageFrom: ['src/**/*.ts'], // exclude coverage from schematic as it only collects from js (instead of ts)..
     coveragePathIgnorePatterns: ['src/rmSafe.ts'],
-    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
 export default config;
